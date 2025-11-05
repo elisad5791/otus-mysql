25e9f26872c1b11fe8ac5e509f555be06d4e8ee5
elisad5791 <elisad5791@mail.ru>
2025-11-05 22:10:14 +0500

    Add hw7

---
+++ hw7/queries.sql
@@ -0,0 +1,19 @@
+-- запрос суммы очков с группировкой и сортировкой по годам
+SELECT year_game, SUM(points) AS year_points
+FROM  statistic
+GROUP BY year_game
+ORDER BY year_game;
+
+-- cte показывающее тоже самое
+WITH cte AS (
+    SELECT year_game, SUM(points) AS year_points
+    FROM statistic
+    GROUP BY year_game
+)
+SELECT * FROM cte ORDER BY year_game;
+
+-- используя функцию LAG вывести кол-во очков по всем игрокам за текущий год и за предыдущий
+SELECT 
+    player_name, 
+    year_game, points AS current_year_points, 
+    LAG(points) OVER (PARTITION BY player_name ORDER BY year_game) AS previous_year_points;
